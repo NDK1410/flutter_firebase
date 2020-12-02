197947b2d1cf58eebfeb2d49b75eb482ae3b0b3d
NDK1410 <khanh.nd1410@gmail.com>
2020-12-01 19:10:20 -0800

    firebase-cloud-messaging

--- android/app/src/main/java/com/example/flutter_firebase/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_firebase/MainActivity.java
@@ -3,4 +3,8 @@ package com.example.flutter_firebase;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+//  @Override
+//  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//    GeneratedPluginRegistrant.registerWith(flutterEngine);
+//  }
 }
